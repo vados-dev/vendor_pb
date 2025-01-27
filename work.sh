05744fedaf35b536e345e979430dbebdb765aa23
vados-dev <vados@vados.ru>
2025-01-27 05:33:30 +0300

    Update work.sh

--- work.sh
+++ work.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm _data/devices.json
-curl https://raw.githubusercontent.com/PitchBlackRecoveryProject/vendor_pb/pb/pb_devices.json > _data/devices.json
+curl https://raw.githubusercontent.com/vados-dev/vendor_pb/pb-dev/pb_devices.json > _data/devices.json
 git add _data/devices.json
 git -c user.name="PitchBlack-BOT" -c user.email="pitchblackrecovery@gmail.com" commit -m "Added New devices"
 
